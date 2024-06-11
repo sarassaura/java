b3a8b5d67ca391b101e151ec22db134c8c4e0632
sarassaura <sarahyukinonakada@gmail.com>
2024-06-11 19:50:38 -0300

    estruturas de dados

--- estruturadedados/Ex01.java
+++ estruturadedados/Ex01.java
@@ -5,6 +5,21 @@ import java.util.Locale;
 import java.util.Queue;
 import java.util.Scanner;
 
+/*
+ * Escreva um programa Java contendo 
+ * uma Collection Queue (Fila) de Objetos da Classe String, 
+ * para organizar a ordem de chegada dos Clientes de um Banco. 
+ * O programa deverá ter um Menu que aceitará as opções 0, 1, 2 e 3:
+ * 
+ * - 1: Adicionar um novo Cliente na fila. Deve solicitar o nome do Cliente.
+ * - 2: Listar todos os Clientes na fila
+ * - 3: Chamar (retirar) uma pessoa da fila 
+ * - 0: O programa deve ser finalizado. 
+
+ * Caso a fila esteja vazia, o programa deverá informar que a 
+ * fila está vazia ao tentar retirar (chamar) um cliente da fila..
+ */
+
 public class Ex01 {
 
 	public static void main(String[] args) {
@@ -25,7 +40,7 @@ public class Ex01 {
 				System.out.printf("Entre com a opção desejada: ");
 				opcao = sc.nextInt();
 			} while (opcao < 0 || opcao > 3);
-
+			// TODO: adicionar default opção Inválida
 			switch (opcao) {
 			case 1:
 				System.out.printf("Digite o nome: ");
--- estruturadedados/Ex02.java
+++ estruturadedados/Ex02.java
@@ -4,6 +4,21 @@ import java.util.Locale;
 import java.util.Scanner;
 import java.util.Stack;
 
+/*
+ * Escreva um programa Java contendo uma Collection Stack (Pilha) 
+ * de Objetos da Classe String, para organizar a retirada de livros 
+ * em uma pilha. O programa deverá ter um Menu que aceitará as opções 
+ * 0, 1, 2 e 3:
+ * 
+ * -  1: Adicionar um novo livro na pilha. Deve solicitar o nome do livro.
+ * - 2: Listar todos os livros da Pilha
+ * - 3: Retirar um livro da pilha 
+ * - 0: O programa deve ser finalizado. 
+ * 
+ * Caso a pilha esteja vazia e o atendente tente retirar 
+ * um livro da pilha, ele deverá informar que a pilha está vazia.
+ */
+
 public class Ex02 {
 
 	public static void main(String[] args) {
